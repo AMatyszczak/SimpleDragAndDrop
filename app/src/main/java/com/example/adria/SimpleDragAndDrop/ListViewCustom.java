db250e4255e8c374621a4f232d7edffec95b05fd
adria <adrianmatyszczak@gmail.com>
2018-06-15 17:13:47 +0200

    Drag and drop now works with multiple selection.

--- app/src/main/java/com/example/adria/SimpleDragAndDrop/ListViewCustom.java
+++ app/src/main/java/com/example/adria/SimpleDragAndDrop/ListViewCustom.java
@@ -4,12 +4,16 @@ import android.content.ClipData;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.ActionMode;
 import android.view.DragEvent;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.AccelerateInterpolator;
 import android.view.animation.Animation;
 import android.view.animation.TranslateAnimation;
+import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.GridView;
 
@@ -17,14 +21,17 @@ import java.util.ArrayList;
 
 import static android.content.ContentValues.TAG;
 
-public class ListViewCustom extends GridView
+public class ListViewCustom extends GridView implements AdapterView.OnItemLongClickListener
 {
     private final static long ANIMATION_DURATION = 225;
 
+    private boolean isDragging;
     private long mCurrViewId;
     private long mNextItemId;
     private View mCurrView;
     private View mNextView;
+
+    private ActionMode mActionMode;
 
     private AdapterCustom mAdapter;
 
@@ -64,9 +71,11 @@ public class ListViewCustom extends GridView
             viewClick.startDrag(data, new View.DragShadowBuilder(viewClick), viewClick, 0);
             viewClick.setVisibility(INVISIBLE);
 
-            return false;
+            return true;
         }
     };
+
+
 
     private AdapterView.OnTouchListener mOnTouchListener = new AdapterView.OnTouchListener()
     {
@@ -96,6 +105,7 @@ public class ListViewCustom extends GridView
                                 switch (action) {
                                     case DragEvent.ACTION_DRAG_STARTED:
                                         Log.e(TAG, "test: "+mNextItemId +" curr: " + mCurrViewId);
+                                        isDragging = true;
                                         break;
                                     case DragEvent.ACTION_DRAG_LOCATION:
 
@@ -115,6 +125,7 @@ public class ListViewCustom extends GridView
                                         }
                                         break;
                                     case DragEvent.ACTION_DRAG_ENDED:
+                                        isDragging = false;
                                         final View droppedView = (View) event.getLocalState();
                                         droppedView.post(new Runnable(){
                                             @Override
@@ -128,9 +139,13 @@ public class ListViewCustom extends GridView
                                         });
                                         break;
                                     default:
+                                        isDragging = false;
+                                        if(mActionMode!= null)
+                                            mActionMode.finish();
                                         result = false;
                                         break;
                                 }
+                                isDragging = false;
                                 return result;
                             }
                         });
@@ -161,6 +176,11 @@ public class ListViewCustom extends GridView
        compositeListener.registerListener(listener);
     }
 
+    @Override
+    public void setMultiChoiceModeListener(MultiChoiceModeListener listener) {
+        super.setMultiChoiceModeListener(new MultiChoiceModeWrapper(listener));
+    }
+
     private View getViewFromId(long id)
 	{
 		int relativePosition = getFirstVisiblePosition();
@@ -175,5 +195,76 @@ public class ListViewCustom extends GridView
 		}
 		return null;
 	}
+
+    @Override
+    public boolean onItemLongClick(AdapterView<?> adapterView, View view, int index, long l) {
+
+        mCurrViewId = index;
+        ClipData data = ClipData.newPlainText("DragData", "HOPA");
+        view.startDrag(data, new View.DragShadowBuilder(view), view, 0);
+        view.setVisibility(INVISIBLE);
+
+        return true;
+    }
+
+    private class MultiChoiceModeWrapper implements MultiChoiceModeListener {
+        MultiChoiceModeListener mWrapped;
+        boolean isDraggable;
+        boolean selectOnly;
+
+        public MultiChoiceModeWrapper(MultiChoiceModeListener listener) {
+            this.mWrapped = listener;
+            this.selectOnly = false;
+        }
+
+        @Override
+        public boolean onCreateActionMode(ActionMode actionMode, Menu menu) {
+            if (mWrapped.onCreateActionMode(actionMode, menu)) {
+                isDraggable = true;
+                return true;
+            }
+            return false;
+        }
+
+        @Override
+        public boolean onActionItemClicked(ActionMode actionMode, MenuItem item) {
+            return mWrapped.onActionItemClicked(actionMode, item);
+        }
+
+        @Override
+        public boolean onPrepareActionMode(ActionMode actionMode, Menu menu) {
+            mActionMode = actionMode;
+            return mWrapped.onPrepareActionMode(actionMode, menu);
+        }
+
+        @Override
+        public void onDestroyActionMode(ActionMode actionMode) {
+            isDraggable = true;
+            selectOnly = false;
+            mActionMode = null;
+
+        }
+
+        @Override
+        public void onItemCheckedStateChanged(ActionMode mode, int position, long id, boolean checked) {
+            if( isDraggable)
+            {
+                isDraggable = false;
+
+                int item = position - getFirstVisiblePosition();
+                View view = getChildAt(item);
+
+
+                mCurrViewId = position;
+                ClipData data = ClipData.newPlainText("DragData", "HOPA");
+                view.startDrag(data, new View.DragShadowBuilder(view), view, 0);
+                if(selectOnly == false) {
+                    view.setVisibility(INVISIBLE);
+                    selectOnly = true;
+                }
+            }
+            mWrapped.onItemCheckedStateChanged(mode, position, id, checked);
+        }
+    }
 
 }
--- app/src/main/java/com/example/adria/SimpleDragAndDrop/MainActivity.java
+++ app/src/main/java/com/example/adria/SimpleDragAndDrop/MainActivity.java
@@ -4,8 +4,14 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ActionMode;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AbsListView;
 import android.widget.AdapterView;
+import android.widget.GridView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -25,20 +31,53 @@ public class MainActivity extends AppCompatActivity {
                 "item 3", "item 4", "item 5", "item 6", "item 7", "item 8",
                 "item 9", "item 10", "item 11", "item 12", "item 13", "item 14",
                 "item 15","item 16","item 17", "item 18", "item 19",
-                "item 20" , "item 21","item 22","item 23","item 24"
+                "item 20" , "item 21","item 22","item 23"
         ));
-        //Log.e("TAG", "HOLAAAAA2" );
-        grid.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-            @Override
-            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Snackbar.make(grid,"HOLAAAAAAAAAAAA",Snackbar.LENGTH_SHORT).show();
-                Log.e("TAG", "HOLAAAAA" );
-                return true;
-            }
-        });
+
+        grid.setChoiceMode(GridView.CHOICE_MODE_MULTIPLE_MODAL);
+        grid.setMultiChoiceModeListener(new MyMultiChoiceListener());
+        grid.setSelector(android.R.color.darker_gray);
         grid.setAdapter(new AdapterCustom(values, this));
 
 
 
+    }
+
+    private class MyMultiChoiceListener implements AbsListView.MultiChoiceModeListener
+    {
+        @Override
+        public boolean onCreateActionMode(ActionMode actionMode, Menu menu) {
+            MenuInflater inflater = actionMode.getMenuInflater();
+            inflater.inflate(R.menu.contextual_menu,menu);
+            return true;
+        }
+
+        @Override
+        public boolean onPrepareActionMode(ActionMode actionMode, Menu menu) {
+
+            return false;
+        }
+
+        @Override
+        public boolean onActionItemClicked(ActionMode actionMode, MenuItem menuItem) {
+
+            return false;
+        }
+
+        @Override
+        public void onDestroyActionMode(ActionMode actionMode) {
+
+        }
+
+        @Override
+        public void onItemCheckedStateChanged(ActionMode actionMode, int i, long l, boolean b) {
+
+            View view = grid.getAdapter().getView(i,null,grid);
+            view.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+
+            final int checkedItemCount = grid.getCheckedItemCount();
+            actionMode.setTitle(checkedItemCount + " Selected");
+
+        }
     }
 }
